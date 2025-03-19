20bd08d9faf2a4c7e22f9743eefe68374e2464ff
wabisabicode <tretyakov@uni-mainz.de>
2025-03-19 17:02:40 +0100

    rename submit button in URLMapForm

--- yacut/forms.py
+++ yacut/forms.py
@@ -6,4 +6,4 @@ from wtforms.validators import DataRequired, Length, Optional
 class URLMapForm(FlaskForm):
     original_link = URLField(validators=[Length(1, 256), DataRequired()])
     custom_id = URLField(validators=[Length(1, 64), Optional()])
-    submit = SubmitField()
+    submit = SubmitField('Создать')
