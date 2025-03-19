62a8f323aef4950803d6e6df4d7070093ac1e62e
wabisabicode <tretyakov@uni-mainz.de>
2025-03-19 23:46:51 +0100

    fix jsonify in /api/id/

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -3,20 +3,23 @@ from flask import jsonify, request
 from . import app, db
 from .error_handlers import InvalidAPIUsage
 from .models import URLMap
+from .views import get_unique_short_id
 
 
 @app.route('/api/id/', methods=['POST'])
 def create_link():
-    data = request.get_json(silent=True)
+    data = request.get_json()
     if data is None:
         raise InvalidAPIUsage('В запросе отсутствуют обязательные поля')
     if 'url' not in data:
         raise InvalidAPIUsage('\"url\" является обязательным полем!')
+    if 'custom_id' not in data:
+        data['custom_id'] = get_unique_short_id()
     url_map = URLMap()
     url_map.from_dict(data)
     db.session.add(url_map)
     db.session.commit()
-    return jsonify({url_map.to_dict()}), 201
+    return jsonify(url_map.to_dict()), 201
 
 
 @app.route('/api/id/<string:short_id>')
