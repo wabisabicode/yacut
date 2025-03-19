dbed364932a77aefffb975d40c2e32a4233ea29e
wabisabicode <tretyakov@uni-mainz.de>
2025-03-19 16:53:27 +0100

    add interface for redirect to original in view

--- yacut/views.py
+++ yacut/views.py
@@ -1,8 +1,15 @@
-from flask import render_template
+from flask import redirect, render_template
 
 from . import app, db
+from .models import URLMap
 
 
 @app.route('/')
 def index_view():
     return render_template('index.html')
+
+
+@app.route('/<string:short_link>')
+def redirect_to_original(short_link):
+    item = URLMap.query.filter_by(short=short_link).first()
+    return redirect(item.original)
