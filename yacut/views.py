82d7ef94b2a648a2f15f48f845598b7ac978c1b7
wabisabicode <tretyakov@uni-mainz.de>
2025-03-19 20:42:02 +0100

    add get_unique_short_id function

--- yacut/views.py
+++ yacut/views.py
@@ -1,12 +1,23 @@
+import random
+import string
+
 from flask import redirect, render_template
 
 from . import app, db
+from .forms import URLMapForm
 from .models import URLMap
+
+
+def get_unique_short_id(length=6):
+    characters = string.ascii_letters + string.digits
+    random_string = "".join(random.choice(characters) for _ in range(length))
+    return random_string
 
 
 @app.route('/')
 def index_view():
-    return render_template('index.html')
+    form = URLMapForm()
+    return render_template('index.html', form=form)
 
 
 @app.route('/<string:short_link>')
