df0db3806dd904d62418548f1ce887de61924efc
wabisabicode <tretyakov@uni-mainz.de>
2025-03-19 20:25:46 +0100

    fix short_link length

--- yacut/models.py
+++ yacut/models.py
@@ -6,7 +6,7 @@ from yacut import db
 class URLMap(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original = db.Column(db.String(256), unique=True, nullable=False)
-    short = db.Column(db.String(64), unique=True)
+    short = db.Column(db.String(16), unique=True)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow())
 
     def to_dict(self):
