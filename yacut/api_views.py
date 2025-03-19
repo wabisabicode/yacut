269f8ce1b3ca0249f448d6e5422869861b3f3dcc
wabisabicode <tretyakov@uni-mainz.de>
2025-03-19 23:31:08 +0100

    add second api endpoint

--- yacut/api_views.py
+++ yacut/api_views.py
@@ -10,10 +10,18 @@ def create_link():
     data = request.get_json(silent=True)
     if data is None:
         raise InvalidAPIUsage('В запросе отсутствуют обязательные поля')
-    if 'original' not in data:
+    if 'url' not in data:
         raise InvalidAPIUsage('\"url\" является обязательным полем!')
     url_map = URLMap()
     url_map.from_dict(data)
     db.session.add(url_map)
     db.session.commit()
     return jsonify({url_map.to_dict()}), 201
+
+
+@app.route('/api/id/<string:short_id>')
+def get_original_link(short_id):
+    item = URLMap.query.filter_by(short=short_id).first()
+    if item is None:
+        raise InvalidAPIUsage('Указанный id не найден')
+    return jsonify({'url': item.original}), 200
