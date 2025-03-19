c2d58d1c5c268d7e2bfcff874be828a10c94225e
wabisabicode <tretyakov@uni-mainz.de>
2025-03-19 16:41:49 +0100

    develop URLMap model

--- yacut/models.py
+++ yacut/models.py
@@ -4,4 +4,15 @@ from yacut import db
 
 
 class URLMap(db.Model):
-    ...
+    id = db.Column(db.Integer, primary_key=True)
+    original = db.Column(db.String(256), unique=True, nullable=False)
+    short = db.Column(db.String(64), unique=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow())
+
+    def to_dict(self):
+        return dict(
+            id=self.id,
+            original=self.original,
+            short=self.short,
+            timestamp=self.timestamp
+        )
