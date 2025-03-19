f6f6f87670f17e0e1cd8314f36d87997dbd38374
wabisabicode <tretyakov@uni-mainz.de>
2025-03-19 17:19:16 +0100

    set names for form fields

--- yacut/forms.py
+++ yacut/forms.py
@@ -1,9 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import URLField, SubmitField
+from wtforms import StringField, URLField, SubmitField
 from wtforms.validators import DataRequired, Length, Optional
 
 
 class URLMapForm(FlaskForm):
-    original_link = URLField(validators=[Length(1, 256), DataRequired()])
-    custom_id = URLField(validators=[Length(1, 64), Optional()])
+    original_link = StringField(
+        'Длинная ссылка',
+        validators=[Length(1, 256), DataRequired()]
+    )
+    custom_id = URLField(
+        'Ваш вариант короткой ссылки',
+        validators=[Length(1, 64), Optional()]
+    )
     submit = SubmitField('Создать')
