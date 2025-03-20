d113c89b54772755e409fcdddec43d9370f721b9
wabisabicode <tretyakov@uni-mainz.de>
2025-03-20 15:31:42 +0100

    fix magic numbers in forms.py

--- yacut/forms.py
+++ yacut/forms.py
@@ -2,14 +2,16 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, URLField
 from wtforms.validators import DataRequired, Length, Optional
 
+from .constants import ORIGINAL_LINK_MAX_LEN, SHORT_LINK_MAX_LEN
+
 
 class URLMapForm(FlaskForm):
     original_link = StringField(
         'Длинная ссылка',
-        validators=[Length(1, 256), DataRequired()]
+        validators=[Length(1, ORIGINAL_LINK_MAX_LEN), DataRequired()]
     )
     custom_id = URLField(
         'Ваш вариант короткой ссылки',
-        validators=[Length(1, 16), Optional()]
+        validators=[Length(1, SHORT_LINK_MAX_LEN), Optional()]
     )
     submit = SubmitField('Создать')
