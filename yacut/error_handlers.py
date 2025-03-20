629be609aabdbc0be6d7e82d2f98749809d0b566
wabisabicode <tretyakov@uni-mainz.de>
2025-03-20 15:36:04 +0100

    move error templates into a separate folder

--- yacut/error_handlers.py
+++ yacut/error_handlers.py
@@ -23,10 +23,10 @@ def invalid_api_usage(error):
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template('404.html'), 404
+    return render_template('errors/404.html'), 404
 
 
 @app.errorhandler(500)
 def internal_error(error):
     db.session.rollback()
-    return render_template('500.html'), 500
+    return render_template('errors/500.html'), 500
